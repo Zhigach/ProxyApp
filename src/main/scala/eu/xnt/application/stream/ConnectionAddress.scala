de7fd7d7004ed31a3d1de4d1ab08a421b738e771
Alexander Zhigachev <sashazhig@gmail.com>
2024-06-25 12:50:05 +0300

    ACtor based StreamReader.scala

--- src/main/scala/eu/xnt/application/stream/ConnectionAddress.scala
+++ src/main/scala/eu/xnt/application/stream/ConnectionAddress.scala
@@ -2,18 +2,19 @@ package eu.xnt.application.stream
 
 import java.io.{IOException, InputStream}
 import java.net.{InetSocketAddress, Socket}
+import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
 final case class ConnectionAddress(host: String, port: Int) {
-    def getStream: Try[InputStream] = {
+    def getStream: Future[InputStream] = {
         val socket = new Socket
         val endpoint = new InetSocketAddress(host, port)
         try {
             socket.connect(endpoint)
             val in = socket.getInputStream
-            Success(in)
+            Future.successful(in)
         } catch
             case ex: IOException =>
-                Failure(ex)
+                Future.failed(ex)
     }
 }
--- src/main/scala/eu/xnt/application/stream/StreamReader.scala
+++ src/main/scala/eu/xnt/application/stream/StreamReader.scala
@@ -1,31 +1,79 @@
 package eu.xnt.application.stream
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, Props, SupervisorStrategy}
+import akka.pattern.pipe
 import Command.*
 import eu.xnt.application.model.Quote
+import eu.xnt.application.stream.StreamReader.{ReadStream, StreamData, StreamFailure}
 
 import java.io.InputStream
 import java.nio.ByteBuffer
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 class StreamReader(connection: ConnectionAddress, quoteReceiver: ActorRef) extends Actor with ActorLogging {
+    
+    implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+    override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+        case _ : RuntimeException => SupervisorStrategy.Restart
+    }
+
+    override def preStart(): Unit = {
+        self ! Connect(connection)
+    }
+
+    private def connected(inputStream: InputStream): Receive = {
+        case ReadStream =>
+            readStream(inputStream).pipeTo(self)
+        case StreamData(byteBuffer) =>
+            quoteReceiver ! Quote.fromByteBuffer(byteBuffer)
+            self ! ReadStream
+        case StreamFailure(exception) =>
+            log.error(s"Exception occurred during connection: ${exception.getMessage}")
+            Thread.sleep(5000)
+            self ! Connect(connection)
+    }
+
+    private def readStream(inputStream: InputStream): Future[StreamData] = {
+        try {
+            val msgLength: Short = ByteBuffer.wrap(inputStream.readNBytes(2)).getShort
+            val messageBuffer: ByteBuffer = ByteBuffer.wrap(inputStream.readNBytes(msgLength))
+            Future.successful(StreamData(messageBuffer))
+        } catch
+            case exception: RuntimeException =>
+                Future.failed(exception)
+    }
+
+    private def disconnected(): Receive = {
+        case Connect(inputStream) =>
+            connect().pipeTo(self)
+    }
+
+    override def postStop(): Unit = {
+        log.info(s"${self.path.name} stopped")
+    }
 
     def getCandleRepository: ActorRef = quoteReceiver
     
-    def connect(): Unit = {
-        val inputStream = connection.getStream
-        inputStream match
-            case Success(value) =>
+    def connect(): Future[InputStream] = {
+        connection.getStream.map {
+            inputStream =>
                 log.info(s"Stream connected")
-                startReadingStream(value)
-            case Failure(exception) =>
+                context.become(connected(inputStream))
+                self ! ReadStream
+                inputStream
+        } recoverWith {
+            case exception =>
                 log.error(s"Exception occurred during connection: ${exception.getMessage}")
                 Thread.sleep(5000)
-                self ! Connect(connection)
+                self ! Connect(connection)                
+                Future.failed(new RuntimeException("Reconnecting due to failure", exception))
+        }
     }
 
-    private def startReadingStream(inputStream: InputStream): Unit = {        
+    private def startReadingStream(inputStream: InputStream): Unit = {
         var streamIsActive = true
 
         while streamIsActive do
@@ -42,13 +90,16 @@ class StreamReader(connection: ConnectionAddress, quoteReceiver: ActorRef) exten
         log.info("Finished reading stream")
     }
 
-    override def receive: Receive = {
-        case Connect(connection) =>
-            log.info("Connecting to stream at {}:{}", connection.host, connection.port);
-            connect()
-        case Disconnect(connection) =>
-            log.info("Disconnecting...") //FIXME no real disconnection
-        case _ => log.error("Unknown message received")
-    }
+    override def receive: Receive = disconnected()
 
+}
+
+object StreamReader {
+    def props(connection: ConnectionAddress, quoteReceiver: ActorRef): Props =
+        Props(new StreamReader(connection, quoteReceiver))
+
+    case class Connect(connection: ConnectionAddress)
+    case object ReadStream
+    case class StreamData(messageBuffer: ByteBuffer)
+    case class StreamFailure(exception: Throwable)
 }
